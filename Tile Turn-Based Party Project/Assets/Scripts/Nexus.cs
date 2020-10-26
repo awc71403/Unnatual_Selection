403193be28c46fcd547bf3f239c401a4cb062c73
LevanaZhang <levanazhang129@gmail.com>
2020-10-25 17:05:02 -0700

    Created Nexus tile and rudimentary Summon button to test nexus tile with

---
+++ Tile Turn-Based Party Project/Assets/Scripts/Nexus.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Nexus : TileBehavior
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Successful Initialization");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnPointerClick(PointerEventData data)
+    {
+        GameManager.GetSingleton().GetSummonUI().gameObject.SetActive(true);
+        Debug.Log("Opened Summon UI");
+    }
+}
